1e7d4b13c34cdd9c502848f8f9b64812b2cb8be0
ACHultman <ahultman0@gmail.com>
2021-05-03 16:48:04 -0700

    test: add home, search, nominations tests

---
+++ setupTests.ts
@@ -0,0 +1,4 @@
+import React from 'react'
+import '@testing-library/jest-dom'
+React.useLayoutEffect = React.useEffect
+require('jest-fetch-mock').enableMocks()
--- test/pages/index.test.tsx
+++ test/pages/index.test.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
-import { render } from '../testUtils'
+import { render, waitForElementToBeRemoved } from '../testUtils'
 import { Home } from '../../pages/index'
+import { screen, fireEvent } from '@testing-library/dom'
+import { getPage } from 'next-page-tester'
 
 jest.mock('../../components/movies/Movie', () => 'mocked-movie')
 
@@ -8,5 +10,32 @@ describe('Home page', () => {
   it('matches snapshot', () => {
     const { asFragment } = render(<Home />, {})
     expect(asFragment()).toMatchSnapshot()
+  })
+
+  it('renders page title', async () => {
+    const { render } = await getPage({
+      route: '/',
+    })
+    render()
+    await waitForElementToBeRemoved(() => screen.getByText(/Loading\.\.\./i))
+    expect(screen.getByText('Welcome To The Shoppies!')).toBeInTheDocument()
+  })
+
+  it('handles search button click by routing to searchg page', async () => {
+    const { render } = await getPage({
+      route: '/',
+    })
+    render()
+    //await waitForElementToBeRemoved(() => screen.getByText(/Loading\.\.\./i))
+
+    const button = screen.getAllByText((context, element) => {
+      return (
+        element.tagName.toLocaleLowerCase() === 'button' &&
+        element.innerHTML === 'Search For Movies'
+      )
+    })
+
+    fireEvent.click(button[0])
+    await screen.findByPlaceholderText(/Search movies by name../i)
   })
 })
