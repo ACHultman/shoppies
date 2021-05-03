03caba5f66c5992870e8de0beeb791539f421bd8
ACHultman <ahultman0@gmail.com>
2021-05-02 20:34:25 -0700

    feat: add nominations component

---
+++ components/Nominations.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { IMovieLong } from '../utils/types'
+import Movie from './movies/Movie'
+
+const Nominations = ({
+  nominations,
+}: {
+  nominations: IMovieLong[]
+}): JSX.Element => {
+  return (
+    <div>
+      <div>
+        <h1>Nominations</h1>
+      </div>
+
+      <div className="flex flex-col">
+        {nominations &&
+          nominations.map((nom, idx) => {
+            return (
+              <div key={idx} className="flex">
+                <div>
+                  <Movie
+                    imdbID={nom.imdbID}
+                    Poster={nom.Poster}
+                    Title={nom.Title}
+                    Year={nom.Year}
+                    Type={nom.Type}
+                  />
+                </div>
+                <p>{nom.Plot}</p>
+              </div>
+            )
+          })}
+      </div>
+    </div>
+  )
+}
+
+export default Nominations
