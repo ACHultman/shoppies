ff61022f1f155837f9c0f2ad2e145a46cf09fe60
ACHultman <ahultman0@gmail.com>
2021-05-03 13:20:38 -0700

    feat: add banner routing

--- components/header/Banner.tsx
+++ components/header/Banner.tsx
@@ -1,16 +1,23 @@
+import { useRouter } from 'next/router'
 import { useSelector } from 'react-redux'
 
 const Banner = (): JSX.Element => {
   const nominations = useSelector((state) => state.nominations.nominations)
+  const router = useRouter()
   if (!nominations || nominations.length < 5) {
     return <></>
   }
   return (
     <div className="text-center">
       <p className="mb-4 stroke-2">Nomination Limit Reached</p>
-      <button className="bg-white text-gray-800 font-bold rounded-full py-4 px-8 shadow-lg uppercase tracking-wider">
-        Review Nominations
-      </button>
+      {router.route !== '/nominations' && (
+        <button
+          onClick={() => router.push('/nominations')}
+          className="bg-white text-gray-800 focus:outline-none font-bold rounded-full py-4 px-8 shadow-lg uppercase tracking-wider"
+        >
+          Review Nominations
+        </button>
+      )}
     </div>
   )
 }
